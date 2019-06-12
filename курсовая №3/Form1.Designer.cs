7e8dc3532b2f201517cfe69b84cb881774b5bfb5
Kotes <51745529+KotesUA@users.noreply.github.com>
2019-06-12 14:25:42 +0300

    Добавьте файлы проекта.

---
+++ курсовая №3/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace курсовая
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.overlay = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EquationTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(58, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(510, 390);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(44, 454);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(536, 42);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "График функции";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // overlay
+            // 
+            this.overlay.AutoSize = true;
+            this.overlay.Location = new System.Drawing.Point(45, 432);
+            this.overlay.Name = "overlay";
+            this.overlay.Size = new System.Drawing.Size(136, 17);
+            this.overlay.TabIndex = 8;
+            this.overlay.Text = "Наложение графиков";
+            this.overlay.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 409);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "f(x) = ";
+            // 
+            // EquationTextBox
+            // 
+            this.EquationTextBox.Location = new System.Drawing.Point(45, 406);
+            this.EquationTextBox.Name = "EquationTextBox";
+            this.EquationTextBox.Size = new System.Drawing.Size(535, 20);
+            this.EquationTextBox.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 507);
+            this.Controls.Add(this.EquationTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.overlay);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Курсовая №3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox overlay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox EquationTextBox;
+    }
+}
+
---
+++ курсовая №3/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace курсовая
+{
+    public partial class Form1 : Form
+    {
+        // создаем рабочую область рисунка
+        Bitmap b2 = new Bitmap(780, 540);
+
+        List<Point> Points = new List<Point>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // рисуем координатную плоскость
+        public void Sis_coor()
+        {
+            // переменные проверки на чётность
+            int chet = 2;
+            int chet2 = 2;
+            pictureBox1.BackColor = Color.White;
+            Pen p = new Pen(Color.Black, 2);
+            Graphics g1 = Graphics.FromImage(b2);
+            for (int i = 0; i <= pictureBox1.Width; i += 20)
+            {
+                g1.DrawLine(Pens.Gray, i, 0, i, pictureBox1.Height);
+                g1.DrawLine(p, i, pictureBox1.Height - pictureBox1.Height / 2 - 4, i, pictureBox1.Height - pictureBox1.Height / 2 + 4);
+                if (chet2 % 2 == 0)
+                {
+                    g1.DrawString(Convert.ToString(-i / 40), new Font("Arial", 8), Brushes.Black, pictureBox1.Width / 2, pictureBox1.Height - pictureBox1.Height / 2 + i);      // подписи осей
+                    g1.DrawString(Convert.ToString(i / 40), new Font("Arial", 8), Brushes.Black, pictureBox1.Width / 2, pictureBox1.Height - pictureBox1.Height / 2 - i);
+                }
+                chet2++;
+            }
+
+            // в цикле с шагом 20 пикелей рисуем прямые
+            for (int j = 0; j <= pictureBox1.Height; j += 20)
+            {
+                g1.DrawLine(Pens.Gray, 0, j, pictureBox1.Width, j);
+                g1.DrawLine(p, pictureBox1.Width / 2 - 4, j, pictureBox1.Width / 2 + 4, j);
+                if (chet % 2 == 0)
+                {
+                    g1.DrawString(Convert.ToString(j / 40), new Font("Arial", 8), Brushes.Black, pictureBox1.Width / 2 + j, pictureBox1.Height - pictureBox1.Height / 2);
+                    g1.DrawString(Convert.ToString(-j / 40), new Font("Arial", 8), Brushes.Black, pictureBox1.Width / 2 - j, pictureBox1.Height - pictureBox1.Height / 2);
+                }
+                chet++;
+            }
+            // ось ординат
+            g1.DrawLine(p, pictureBox1.Width / 2, 0, pictureBox1.Width / 2, pictureBox1.Height);
+            // ось абсцисс
+            g1.DrawLine(p, 0, pictureBox1.Height - pictureBox1.Height / 2, pictureBox1.Width, pictureBox1.Height - pictureBox1.Height / 2);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // рисуем систему координат
+            Sis_coor();
+            pictureBox1.Image = b2;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double f0 = -0.1666;
+            double x0 = -0.5;
+            double f;
+            Pen p = new Pen(Color.Blue, 4);
+            Bitmap b1 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g1 = Graphics.FromImage(b2);
+            if (overlay.Checked)    // проверка режима рисования
+            { Sis_coor(); }
+            else
+            {
+                g1.Clear(Color.White);
+                Sis_coor();
+            }
+            double x = -0.48;
+            while (x <= 4)
+            {
+                if (x != 1)
+                {
+                    f = (x * x * x) / ((x * x) - 1);
+                    g1.DrawLine(p, Convert.ToInt64(x0 * 40 + pictureBox1.Width / 2), Convert.ToInt64(f0 * 40 + pictureBox1.Height - pictureBox1.Height / 2), Convert.ToInt64(x * 40 + pictureBox1.Width / 2), Convert.ToInt64(f * 40 + pictureBox1.Height - pictureBox1.Height / 2));
+                    f0 = f;
+                    x0 = x;
+                }
+                x += 0.2;
+            }
+            pictureBox1.Image = b2;        // изменяем отображаемую картинку
+        }
+    }
+}
